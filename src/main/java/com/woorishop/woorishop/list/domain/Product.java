720f516067d66349fe31a1eb930df6b3fc797dd7
이정민 <ijeong0416@gmail.com>
2024-01-09 18:19:31 +0900

    Draft

---
+++ src/main/java/com/woorishop/woorishop/list/domain/Product.java
@@ -0,0 +1,10 @@
+package com.woorishop.woorishop.list.domain;
+
+// @AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+    private String category;
+    
+}
