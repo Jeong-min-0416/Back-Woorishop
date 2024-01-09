8ae94d2a8d6f53fd96f76fa6d2a2404e18dbbc84
이정민 <ijeong0416@gmail.com>
2024-01-09 18:08:05 +0900

    Draft

---
+++ src/main/java/com/woorishop/woorishop/WoorishopApplication.java
@@ -0,0 +1,13 @@
+package com.woorishop.woorishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorishop/woorishop/WoorishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorishop.woorishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
